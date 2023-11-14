3991272ca2be5cd1f1b115dff364e3181d2f54e5
David Garay <davidgaraycr@gmail.com>
2023-11-14 01:31:11 -0500

    free-version corriendo como app separada y algunas cards no se muestran correctamente

--- frontend/ecommerce/src/app/app.module.ts
+++ frontend/ecommerce/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 
 // MY COMPONENTS
 import { ProductoComponent } from './producto/producto.component';
+import { LoginComponent } from './login/login.component';
+
 
 // PRIME NG MODULES
 import { TableModule } from 'primeng/table';
@@ -19,16 +21,48 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 
+// PLANTILLA DASHBOARD
+import { AdminComponent } from './theme/layout/admin/admin.component';
+import { NavigationItem } from './theme/layout/admin/navigation/navigation';
+import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
+import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
+import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
+import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
+import { NavLogoComponent } from './theme/layout/admin/nav-bar/nav-logo/nav-logo.component';
+import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
+import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
+import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
+import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
+import { SharedModule } from './theme/shared/shared.module';
+import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
+import { GuestComponent } from './theme/layout/guest/guest.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    LoginComponent,
+    
+    // PLANTILLA
+    AdminComponent,
+    NavBarComponent,
+    NavLeftComponent,
+    NavRightComponent,
+    NavigationComponent,
+    NavLogoComponent,
+    NavContentComponent,
+    NavGroupComponent,
+    NavItemComponent,
+    NavCollapseComponent,
+    ConfigurationComponent,
+    GuestComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     HttpClientModule,
     BrowserAnimationsModule,
     TableModule,
@@ -38,9 +72,11 @@ import { ToastModule } from 'primeng/toast';
     InputTextModule,
     InputNumberModule,
     ConfirmDialogModule,
-    ToastModule
+    ToastModule,
+    
+    // PLANTILLA
   ],
-  providers: [],
+  providers: [NavigationItem],
   bootstrap: [AppComponent]
 })
 
